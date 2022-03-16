364d7127c59ae63a9acd2d152930d36bb2bd0e62
Justin Scorzafava <jkscorzafava@gmail.com>
2022-03-16 13:14:30 -0600

    fixed border-radius issue

--- src/components/FoodOptions.js
+++ src/components/FoodOptions.js
@@ -14,13 +14,14 @@ const Wrapper = styled.section`
   display: grid;
   grid-column: 2 / 4;
   background-color: lightgrey;
-  border-radius: 5rem;
+  border-radius: 5rem 0 0 5rem;
 
   .one {
     background: linear-gradient(rgba(255, 72, 0, 0.704), rgba(255, 72, 0, 0.204));
       grid-column 2 / 3;
     grid-row: 1 / -1;
     border-radius: 5rem;
+
   }
 `;
 
