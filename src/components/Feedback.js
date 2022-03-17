caf0a2f8346463c1aa0a619fd823e838b7c75a8b
Justin Scorzafava <jkscorzafava@gmail.com>
2022-03-17 15:17:02 -0600

    added box shadow to feedbackemail container components to add cool modern looking effect

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -18,6 +18,8 @@ const Wrapper = styled.section`
   justify-content: center;
   align-items: center;
   transform: translateX(-40%);
+  box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+  border-radius: 5rem;
 
   section {
     background: lightgrey;
