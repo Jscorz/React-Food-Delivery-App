274a67b0052b2fbf0ec39c27e2e66df284b26c78
Justin Scorzafava <jkscorzafava@gmail.com>
2022-03-17 13:56:58 -0600

    started building out mock services page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Menu from './components/Menu';
+import Shop from './components/Shop';
+import Service from './components/Service';
 
 function App() {
   return (
@@ -9,9 +12,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path='/' element={<Header />} />
-        <Route path='/menu'>{/* <Menu /> */}</Route>
-        <Route path='/shop'>{/* <Shop /> */}</Route>
-        <Route path='/service'>{/* <Service /> */}</Route>
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/service' element={<Service />} />
       </Routes>
     </Router>
   );
--- src/components/Service.js
+++ src/components/Service.js
@@ -4,7 +4,50 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Service = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <section className='grid'>
+        <article className='grid-second'>
+          <div className='main-text'>
+            Here's our mission statement and why we love our customers!
+          </div>
+        </article>
+      </section>
+    </Wrapper>
+  );
 };
 
 export default Service;
+
+const Wrapper = styled.section`
+  .grid {
+    background-color: lightgrey;
+    min-height: 100vh;
+    max-width: 90vw;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    border-radius: 8%;
+
+    &-second {
+      margin: 2rem;
+      min-width: 95%;
+      background: white;
+      border-radius: 5rem;
+
+      display: grid;
+      grid-template-columns: 40% 1fr 1fr;
+    }
+  }
+
+  .main-text {
+    grid-column: 1 / 2;
+    grid-row: 1 / -1;
+    background: linear-gradient(
+      to right top,
+      rgba(255, 72, 0, 0.8),
+      rgba(255, 72, 0, 0.704)
+    );
+    border-radius: 5rem;
+  }
+`;
