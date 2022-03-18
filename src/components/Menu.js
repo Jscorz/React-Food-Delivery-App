8e5f5c6a52b7e2a7d500ab461b5b03ea2fde4d02
Justin Scorzafava <jkscorzafava@gmail.com>
2022-03-18 09:28:40 -0600

    laid out main text for page and styled it as well as right arrow icon

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from 'react';
 import '../index.css';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { BsArrowRightCircle } from 'react-icons/bs';
 
 const Menu = () => {
   return (
@@ -9,7 +10,15 @@ const Menu = () => {
       <section className='grid'>
         <article className='grid-second'>
           <div className='background'>
-            <main className='card'>card element</main>
+            <main className='card'>
+              <p>No menu from us, but here are some </p>
+              <span>great options</span>
+              <Link to='/Shop'>
+                <button>
+                  <BsArrowRightCircle />
+                </button>
+              </Link>
+            </main>
           </div>
         </article>
       </section>
@@ -57,11 +66,46 @@ const Wrapper = styled.section`
   }
 
   .card {
-    background-color: grey;
+    background-color: lightgrey;
     min-height: 55vh;
     width: 65vw;
     border-radius: 5rem;
 
     transform: translateX(20%);
+    text-transform: capitalize;
+  }
+
+  .card {
+    font-size: 4rem;
+    font-weight: 800;
+
+    color: rgba(0, 0, 0, 0.65);
+    padding: 2rem;
+
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+  }
+
+  button {
+    color: rgba(255, 72, 0, 0.6);
+    font-size: 3.5rem;
+
+    background: transparent;
+    border: none;
+    margin-left: 2rem;
+    cursor: pointer;
+    transition: all 0.2s;
+
+    &:hover {
+      transform: translateX(3%);
+      color: rgba(255, 72, 0, 0.9);
+    }
+  }
+
+  span {
+    color: rgba(255, 72, 0, 0.6);
   }
 `;
