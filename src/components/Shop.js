6cde585ce1a86f7de26227e710c678b9b9bd1f06
Justin Scorzafava <jkscorzafava@gmail.com>
2022-03-19 10:30:20 -0600

    styled font color/size/positioning on cards

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -11,71 +11,66 @@ const Shop = () => {
       <section className='grid'>
         <article className='grid-second'>
           <div className='background'>
-            <section className='card-container'>
-              <div className='img-container'>
-                <img src={tacobell} alt='taco bell' />
-              </div>
-              <div className='text-container'>
-                <h3>Taco Bell</h3>
-                <div className='text-container-second'>
-                  <h5>0.4mi</h5>
-                  <h5>11 min</h5>
-                  <h5>$3.99 delivery fee</h5>
-                </div>
-              </div>
-            </section>
-            <section className='card-container'>
-              <div className='img-container'>
-                <img src={tacobell} alt='taco bell' />
-              </div>
-              <div className='text-container'>
-                <h3>Taco Bell</h3>
-                <div className='text-container-second'>
-                  <h5>0.4mi</h5>
-                  <h5>11 min</h5>
-                  <h5>$3.99 delivery fee</h5>
-                </div>
-              </div>
-            </section>
-            <section className='card-container'>
-              <div className='img-container'>
-                <img src={tacobell} alt='taco bell' />
-              </div>
-              <div className='text-container'>
-                <h3>Taco Bell</h3>
-                <div className='text-container-second'>
-                  <h5>0.4mi</h5>
-                  <h5>11 min</h5>
-                  <h5>$3.99 delivery fee</h5>
-                </div>
-              </div>
-            </section>
-            <section className='card-container'>
-              <div className='img-container'>
-                <img src={tacobell} alt='taco bell' />
-              </div>
-              <div className='text-container'>
-                <h3>Taco Bell</h3>
-                <div className='text-container-second'>
-                  <h5>0.4mi</h5>
-                  <h5>11 min</h5>
-                  <h5>$3.99 delivery fee</h5>
-                </div>
-              </div>
-            </section>
-            <section className='card-container'>
-              <div className='img-container'>
-                <img src={tacobell} alt='taco bell' />
-              </div>
-              <div className='text-container'>
-                <h3>Taco Bell</h3>
-                <div className='text-container-second'>
-                  <h5>0.4mi</h5>
-                  <h5>11 min</h5>
-                  <h5>$3.99 delivery fee</h5>
-                </div>
-              </div>
-            </section>
+            <div className='main-container'>
+              <main className='column-1'>
+                <section className='card-container-2'>
+                  <div className='img-container'>
+                    <img src={tacobell} alt='taco bell' />
+                  </div>
+                  <div className='text-container'>
+                    <h3>Taco Bell</h3>
+                    <div className='text-container-second'>
+                      <h5>0.4mi</h5>
+                      <h5>11 min</h5>
+                      <h5>$3.99 delivery fee</h5>
+                    </div>
+                  </div>
+                </section>
+                <section className='card-container-2'>
+                  <div className='img-container'>
+                    <img src={tacobell} alt='taco bell' />
+                  </div>
+                  <div className='text-container'>
+                    <h3>Taco Bell</h3>
+                    <div className='text-container-second'>
+                      <h5 className='margin-left'>0.4mi</h5>
+                      <h5>11 min</h5>
+                      <h5>$3.99 delivery fee</h5>
+                    </div>
+                  </div>
+                </section>
+              </main>
+              <main className='column-2'>
+                <section className='card-container'>
+                  <div className='img-container'>
+                    <img src={tacobell} alt='taco bell' />
+                  </div>
+                  <div className='text-container'>
+                    <h3>Taco Bell</h3>
+                    <div className='text-container-second'>
+                      <h5>0.4mi</h5>
+                      <h5>11 min</h5>
+                      <h5>$3.99 delivery fee</h5>
+                    </div>
+                  </div>
+                </section>
+                <section className='card-container'>
+                  <div className='img-container'>
+                    <img src={tacobell} alt='taco bell' />
+                  </div>
+                  <div className='text-container'>
+                    <h3>Taco Bell</h3>
+                    <div className='text-container-second'>
+                      <div className='distance-container'>
+                        <h5 className='margin-left'>| 0.4mi </h5>
+                        <h5>| 11 min</h5>
+                      </div>
+                      <h5>$3.99 delivery fee</h5>
+                    </div>
+                  </div>
+                </section>
+              </main>
+            </div>
           </div>
         </article>
       </section>
@@ -125,21 +120,60 @@ const Wrapper = styled.section`
 
   .card-container {
     display: flex;
-    background-color: lightgrey;
-    border-radius: 2rem;
+    cursor: pointer;
+    flex-direction: column;
+    background-color: white;
+    border: 1px solid lightgrey;
+    margin-left: 15vw;
+    margin-top: 10vh;
+  }
+
+  .card-container-2 {
+    display: flex;
+    cursor: pointer;
+    flex-direction: column;
+    background-color: white;
+    border: 1px solid lightgrey;
+    margin-top: 10vh;
+    margin-left: 15vw;
   }
 
   img {
-    border-radius: 2rem;
-    max-height: 25vh;
+    min-width: 20vw;
     object-fit: cover;
     display: block;
   }
 
   .text-container {
     display: flex;
-    padding: 0 3rem;
+    padding: 2rem 2rem 2rem 1rem;
     flex-direction: column;
     justify-content: space-around;
+  }
+
+  .column-1 {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .main-container {
+    display: flex;
+  }
+
+  .distance-container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .margin-left {
+    margin-right: 1.5rem;
+  }
+
+  h3 {
+    color: rgba(0, 0, 0, 0.8);
+  }
+
+  h5 {
+    color: rgba(0, 0, 0, 0.6);
   }
 `;
